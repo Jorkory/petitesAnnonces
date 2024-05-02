1a50c6f803aee137d0afc458f2b01295fd50ca49
Joris Lamaque <jor57lam@gmail.com>
2024-05-02 10:23:49 +0200

    Initialize projet

---
+++ templates/layout/footer.php
@@ -0,0 +1,3 @@
+<footer class="footer">
+    <p class="footer__text">2024 - Petites annonces</p>
+</footer>
---
+++ templates/layout/header.php
@@ -0,0 +1,3 @@
+<header class="header">
+    <h1 class="header__title">Petites annonces</h1>
+</header>
